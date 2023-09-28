906e2eda00c78dbd3f01945272a5f4a56a064929
wayn3r <w9delacruz@gmail.com>
2023-09-27 20:26:48 -0400

    first commit

---
+++ src/main/java/com/unapec/cajaunapec/CajaUnapecApplication.java
@@ -0,0 +1,13 @@
+package com.unapec.cajaunapec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajaUnapecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CajaUnapecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unapec/cajaunapec/CajaUnapecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unapec.cajaunapec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajaUnapecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
